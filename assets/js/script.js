1bab10b728865ada1171bf5d4b32e801de3b3ca3
davide laneri <davidelan@hotmail.com>
2024-01-25 18:51:45 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
               let gameType = this.getAttribute("data-type");
               runGame(gameType);  
@@ -35,8 +35,22 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Check the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
-    
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right bro!!!");
+    } else {
+        alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
